c98b026ea92b98c40c1160a0ca19ce39c784ceea
bshaw50 <bshaw50@bruinmail.slcc.edu>
2026-04-16 14:58:07 -0600

    Update Airport.java
    Add content to airport.

--- src/airportRoute/Airport.java
+++ src/airportRoute/Airport.java
@@ -1,5 +1,11 @@
 package airportRoute;
 
+import java.awt.BorderLayout;
+import java.awt.Color;
+
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
 /**
  * 
  * 
@@ -9,6 +15,14 @@ public class Airport {
 	private String name;
 	private double distance;
 	
+	public static void display(JPanel contentPane) {
+		JPanel southPanel = new JPanel();
+		contentPane.add(southPanel, BorderLayout.SOUTH);
+		
+		JLabel displayRoute = new JLabel("This Work Some Day");
+		displayRoute.setBackground(new Color(255, 255, 255));
+		southPanel.add(displayRoute);
+	}
 	
 	
 }
