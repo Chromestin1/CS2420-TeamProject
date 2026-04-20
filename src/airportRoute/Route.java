1e977d38cae63b6908a9317274f9829313765085
Chromestin1 <83986035+Chromestin1@users.noreply.github.com>
2026-04-20 14:35:34 -0600

    Update Route.java
    Implemented most of the methods

--- src/airportRoute/Route.java
+++ src/airportRoute/Route.java
@@ -1,5 +1,7 @@
 package airportRoute;
 
+import edu.princeton.cs.algs4.StdOut;
+
 /**
  * 
  * 
@@ -11,41 +13,59 @@ public class Route {
 	private Airport origin;
 	private Airport destination;
 	
-	/**
-	 * 
-	 * 
-	 * @return the route
-	 */
-	public Route Route() {
-		return null;
+	
+	public Route(Airport origin, Airport destination, double cost) {
+		this.origin = origin;
+		this.destination = destination;
+		this.cost = cost;
+		this.shortRoute = this;
 	}
 	
 	/**
+	 * Calculates the shortest path
 	 * 
+	 * For now, this compares the current route cost with the stored shortRoute
+	 * and updates it if this route is cheaper.
 	 */
 	public void calcPath() {
-		
+		if (shortRoute == null || this.cost < shortRoute.cost)
+			shortRoute = this;
 	}
 	
 	/**
+	 * Updates the cost of the trip.
 	 * 
-	 * 
-	 * @param newCost
+	 * @param newCost the new route cost
 	 * @return the updatedCost of the trip
+	 * @throws IllegalArgumentException if newCost is negative
 	 */
 	public double updateCost(Double newCost) {
-		return 0;
+		if (newCost == null || newCost < 0)
+			throw new IllegalArgumentException("Cost must be non-negative");
+		
+		cost  = newCost;
+		return cost;
 	}
 	
-	public void CheckPassenger() {
-	
+	/*
+	 * Checks whether a passenger can take this route.
+	 * 
+	 * 
+	 */
+	public void checkPassenger() {
+		if (origin == null || destination == null)
+			throw new IllegalArgumentException("Route must have an origin and destination");
 	}
 	
 	/**
 	 * Books the trip
 	 */
 	public void book() {
+		checkPassenger();
 		
+		StdOut.println("Trip booked from " + origin + " to " + destination + " for $" + cost);
 	}
+	
+	
 	
 }
