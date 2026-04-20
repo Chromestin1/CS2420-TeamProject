43065186eb02613cf2e76240b1c4b936757c4e7a
Chromestin1 <83986035+Chromestin1@users.noreply.github.com>
2026-04-20 14:34:28 -0600

    Update MapWindow.java
    Implemented icon and zoom methods

--- src/airportRoute/MapWindow.java
+++ src/airportRoute/MapWindow.java
@@ -7,6 +7,8 @@ import javax.swing.ImageIcon;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 
+import edu.princeton.cs.algs4.StdOut;
+
 /**
  * MapWindow is the widow that shows the map for the Window
  * It has 2 methods being zoom and icon.
@@ -15,24 +17,31 @@ import javax.swing.JPanel;
  */
 public class MapWindow {
 	private String city;
+	private int zoomLevel;
 	
 	/**
 	 * 
 	 * @param inOrOut the positive or negative zoom value
 	 */
+	
+	public MapWindow(String city) {
+		this.city = city;
+		this.zoomLevel = 100;
+	}
+	
 	public void zoom(int inOrOut) {
-		if (inOrOut >= 0) {
-			
-		} else if (inOrOut <= 0) {
-			
-		}
+		zoomLevel += inOrOut;
+		
+		if (zoomLevel >= 10)
+			zoomLevel = 10;
+		
 	}
 	
 	/**
 	 * The point of a city
 	 */
 	public void icon() {
-		
+		StdOut.println("Displaying icon for " + city);
 	}
 	
 	public static void display(JPanel contentPane) {
